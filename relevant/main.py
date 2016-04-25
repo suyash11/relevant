ee94f4747465401a740353328c5ba00deb0f162b
suyash shukla <suyashshukla2811@gmail.com>
2016-04-25 15:34:51 +0530

    description file updated

--- relevant/main.py
+++ relevant/main.py
@@ -1,10 +1,39 @@
+#!/usr/bin/python
 """Main class file, used to aggreagate all modules."""
 
-import sys
+import argparse
 import csv_to_list
 import cosine_similarity
 import constants
 import object_dict
+
+__author__ = 'Suyash Shukla'
+
+
+def get_args():
+    """The function parses and return arguments passed."""
+    # Assign description to the help doc
+    parser = argparse.ArgumentParser(
+        description="""Script retrieves receives the csv file and
+                       outputs the relevant news articles""")
+    # Add arguments
+    parser.add_argument(
+        '-c', '--csv_file',
+        help='CSV file path as a string', required=False,
+        nargs='?', default=constants.csv_file,
+        type=str)
+    parser.add_argument(
+        '-s', '--input_string', type=str,
+        help='Input string for which relevant news to be searched',
+        required=False, default=constants.input_string)
+    parser.add_argument(
+        '-n', '--number_of_results', type=int,
+        help='Number of search results to be found', required=False,
+        default=constants.number_of_results)
+    # Array for all arguments passed to script
+    args = parser.parse_args()
+    # Return all variable values
+    return args.csv_file, args.input_string, args.number_of_results
 
 
 def relevancy_controller(csv_file=None,
@@ -34,10 +63,6 @@ def relevancy_controller(csv_file=None,
     """
     csv_news_list = csv_to_list.csv_parser(csv_file=csv_file)
     article_set = set()
-    if not input_string:
-        input_string = constants.input_string
-    if not number_of_results:
-        number_of_results = constants.number_of_results
     article_set.add(input_string)
     csv_news_list = csv_news_list[1:]
     for item in csv_news_list:
@@ -52,6 +77,7 @@ def relevancy_controller(csv_file=None,
     return return_s
 
 if __name__ == '__main__':
-    relevancy_controller(csv_file=sys.argv[1],
-                         input_string=sys.argv[2],
-                         number_of_results=sys.argv[3])
+    csv_file, input_string, number_of_results = get_args()
+    relevancy_controller(csv_file=csv_file,
+                         input_string=input_string,
+                         number_of_results=number_of_results)
